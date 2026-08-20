66032f72857fd5fb2bef46ece9d8c994203b10de
Jayesh <jayeshmjk24@gmail.com>
2026-08-20 20:55:10 +0530

    added database and validation

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,19 @@
+const Task= require("../models/tasks.js")
 const getAlltasks=(req,res) => {
     res.send('all items');
 }
 
-const createTask= (req,res) => {
-    res.send("create task");
+const createTask= async(req,res) => {
+    try{
+    const task= await Task.create(req.body);
+    res.status(201).json({task});
+    }
+    catch {
+        res.status(500).json({msg:error});
+    }
 }
 const getTask= (req,res) => {
-    res.send("get task");
+    res.json({id:req.params.id});
 }
 const updateTask= (req,res) => {
     res.send("update task");
